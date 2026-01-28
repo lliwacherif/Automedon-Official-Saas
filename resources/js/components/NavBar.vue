933eb35c73ea778f59e07cbe1735de619188727c
Liwa CHERIF <l.cherif@vaerdia.com>
2026-01-28 15:56:08 +0100

    style: Change desktop logout button to icon only

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -147,8 +147,10 @@ async function handleLogout() {
                         <RouterLink v-if="!authStore.isAdmin" to="/client/reservations" class="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium mr-2">
                             {{ $t('nav.my_bookings') }}
                         </RouterLink>
-                        <button @click="handleLogout" class="text-red-600 hover:text-red-800 px-3 py-2 rounded-md text-sm font-medium">
-                            {{ $t('nav.logout') }}
+                        <button @click="handleLogout" class="text-red-600 hover:text-red-800 p-2 rounded-full hover:bg-red-50 transition-colors" :title="$t('nav.logout')">
+                            <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                            </svg>
                         </button>
                     </template>
                     <template v-else>
